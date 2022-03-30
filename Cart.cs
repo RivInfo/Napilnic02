d29a84e3d062e9d12e69b201d8fce35ac9596d2c
Роман Иванов <romaiv99@gmail.com>
2022-03-30 14:32:56 +0300

    Test1

---
+++ Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnic02
+{
+    class Cart : ProductsContainer
+    {
+        public string Paylink { get; private set; } = "Купить";
+
+        public Good[] Order()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Good.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnic02
+{
+    class Good
+    {
+        public readonly string Id;
+
+        public Good(string name)
+        {
+            Id = name;
+        }
+    }
+}
